9c112744935cc54658f3309dd77377a921b23006
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-04-07 20:37:09 +0300

    Homework object&prototype comleted

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,33 @@
+'use strict'
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    } else {
+        let animalSound = animal.sound;
+        return animalSound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let averageMark;
+    if (marks.length == 0) {        
+        return 0;
+    } else {
+        let sum = 0;  
+        for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+        }
+        averageMark = Math.round(sum / marks.length);
+        return averageMark;  
+    }    
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date();
+    let date = new Date(birthday);
+    let diff = now.getTime() - date.getTime();
+    let age = (diff - 86400000 * 5) / 31536000000; //86400000 * 5 - учёт високосных годов
+    //(в 18 лет 5 високосных годов, 86400000мс в сутках)
+    console.log(age);
+    return age > 18 ? true : false;
 }
